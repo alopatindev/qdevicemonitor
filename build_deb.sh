e5bc8e049e648f1f51b5b80c3f0fd06c31300d1c
Alexander Lopatin <alopatindev@gmail.com>
2014-12-13 00:44:59 +0300

    deployment attempt

--- build_deb.sh
+++ build_deb.sh
@@ -3,12 +3,16 @@
 set -e
 
 REVISION=$(git rev-parse HEAD)
-VERSION="0.0-1"
+VERSION="0.0.1"
 PROGRAMNAME="qdevicemonitor"
+
+# round((size in bytes)/1024)
+INSTALLED_SIZE=$(du -s ./debian/usr | awk '{x=$1/1024; i=int(x); if ((x-i)*10 >= 5) {f=1} else {f=0}; print i+f}')
 
 mkdir -p ./debian/DEBIAN
 mkdir -p ./debian/usr/bin
 find ./debian -type d | xargs chmod 755
+cp -v "${PROGRAMNAME}/${PROGRAMNAME}" ./debian/usr/bin/
 
 echo "Package: ${PROGRAMNAME}
 Version: ${VERSION}
@@ -16,6 +20,7 @@ Section: contrib
 Priority: optional
 Architecture: amd64
 Depends: libqt5widgets5 (>= 5.0.0)
+Installed-Size: ${INSTALLED_SIZE}
 Maintainer: Alexander Lopatin <alexlopatin ät gmail.com>
 Description: Crossplatform Android, iOS and text file log viewer
  This programs shows a log from devices that run Android and iOS.
