4d10e1b730e5f7d5f5c06d3a085ebe4aa04c6c97
Alexander Lopatin <alopatindev@gmail.com>
2014-12-16 23:14:31 +0300

    packaging a directory when deploying for windows

--- ci-scripts/set_env.bat
+++ ci-scripts/set_env.bat
@@ -3,4 +3,5 @@ set MINGW=C:\MinGW
 set PATH=%PATH%;%QTDIR%\bin;%MINGW%\bin;C:\Program Files\7-Zip
 set OUTPUT_DATETIME=%date:~-4,4%%date:~-10,2%%date:~-7,2%-%time:~0,2%-%time:~3,2%-%time:~6,2%
 set OUTPUT_DATETIME=%OUTPUT_DATETIME: =0%
-set OUTPUT_FILENAME=qdevicemonitor-%OUTPUT_DATETIME%-%APPVEYOR_REPO_COMMIT:~0,8%.zip
+set OUTPUT_DIRECTORY=qdevicemonitor-%OUTPUT_DATETIME%-%APPVEYOR_REPO_COMMIT:~0,8%
+set OUTPUT_FILENAME=%OUTPUT_DIRECTORY%.zip
