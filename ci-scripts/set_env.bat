23109292d7ffee483cedbd87b49c2083624826ee
Alexander Lopatin <alopatindev@gmail.com>
2014-12-19 16:28:29 +0300

    updated ci scripts

--- ci-scripts/set_env.bat
+++ ci-scripts/set_env.bat
@@ -1,7 +1,8 @@
 set QTDIR=C:\Qt\Qt5.3.2\5.3\mingw482_32
 set MINGW=C:\MinGW
 set PATH=%PATH%;%QTDIR%\bin;%MINGW%\bin;C:\Program Files\7-Zip
+set PROGRAMNAME=qdevicemonitor
 set OUTPUT_DATETIME=%date:~-4,4%%date:~-10,2%%date:~-7,2%-%time:~0,2%-%time:~3,2%-%time:~6,2%
 set OUTPUT_DATETIME=%OUTPUT_DATETIME: =0%
-set OUTPUT_DIRECTORY=qdevicemonitor-%OUTPUT_DATETIME%-%APPVEYOR_REPO_COMMIT:~0,8%
+set OUTPUT_DIRECTORY=%PROGRAMNAME%-%OUTPUT_DATETIME%-%APPVEYOR_REPO_COMMIT:~0,8%
 set OUTPUT_FILENAME=%OUTPUT_DIRECTORY%.zip
--- ci-scripts/set_env.sh
+++ ci-scripts/set_env.sh
@@ -3,4 +3,4 @@
 export REVISION=$(git rev-parse HEAD)
 export VERSION="0.0.1"
 export PROGRAMNAME="qdevicemonitor"
-export OUTPUT_ARCHIVE_NAME="${PROGRAMNAME}-${VERSION}-$(date -u +%Y%m%d-%H-%M-%S)-${REVISION:0:8}"
+export OUTPUT_ARCHIVE_NAME="${PROGRAMNAME}-$(date -u +%Y%m%d-%H-%M-%S)-${REVISION:0:8}"
