0234cb3941df08e4ca9af134a7b61e0e54c34cb4
Alexander Lopatin <alopatindev@gmail.com>
2015-11-22 23:02:11 +0300

    output filename: replaced time with build number

--- ci-scripts/set_env.sh
+++ ci-scripts/set_env.sh
@@ -2,11 +2,12 @@
 
 export REVISION_FULL="${TRAVIS_COMMIT:-$(git rev-parse HEAD)}"
 export REVISION="${REVISION_FULL:0:8}"
+export BUILD_NUMBER="${TRAVIS_BUILD_NUMBER:-0}"
 
-export VERSION_WITH_BUILD_NUMBER="${VERSION}\\ build\\ ${TRAVIS_BUILD_NUMBER:-0}\\ rev\\ ${REVISION}"
+export VERSION_WITH_BUILD_NUMBER="${VERSION}\\ build\\ ${BUILD_NUMBER}\\ rev\\ ${REVISION}"
 echo "${VERSION_WITH_BUILD_NUMBER}"
 export PROGRAMNAME="qdevicemonitor"
-export OUTPUT_DIRECTORY="${PROGRAMNAME}-$(date -u +%Y%m%d-%H-%M-%S)-${REVISION}-${CXX}"
+export OUTPUT_DIRECTORY="${PROGRAMNAME}-$(date -u +%Y%m%d)-${BUILD_NUMBER}-${REVISION}-${CXX}"
 export STORAGE_PATH="${STORAGE_HOSTNAME}:/home/frs/project/${PROGRAMNAME}/ci"
 export STORAGE_USER="sbar"
 export STORAGE_USER_AND_PATH="${STORAGE_USER}@${STORAGE_PATH}"
